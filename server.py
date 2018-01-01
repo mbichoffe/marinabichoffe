b1e13af5be54170f5833e789a5e249dbd22ce275
mbichoffe <bichoffe.marina@gmail.com>
2017-12-31 17:03:29 -0800

    updated flask to production

--- server.py
+++ server.py
@@ -1,6 +1,4 @@
 from flask import Flask, jsonify, render_template, request
-import time
-from random import randint
 
 app = Flask(__name__)
 
@@ -13,4 +11,4 @@ def homepage():
     return render_template("index.html")
 
 
-app.run(debug=True,host="0.0.0.0", port=5000)
+app.run()
